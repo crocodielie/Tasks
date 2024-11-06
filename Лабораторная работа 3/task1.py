5e2522f4c1c68f13d44840bdab9f4a1d6969ca88
crocodielie <val5ws7@gmail.com>
2024-11-06 23:21:53 +0300

    Лабораторная работа 3

---
+++ Лабораторная работа 3/task1.py
@@ -0,0 +1,16 @@
+# TODO Напишите функцию для поиска индекса товара
+
+def search_index(scroll, product):
+    for i in range(len(scroll)):
+        if scroll[i] == product:
+            return i
+    return None
+
+items_list = ['яблоко', 'банан', 'апельсин', 'груша', 'киви', 'банан']
+
+for find_item in ['банан', 'груша', 'персик']:
+    index_item = search_index(items_list, find_item) # TODO Вызовите функцию, что получить индекс товара
+    if index_item is not None:
+        print(f"Первое вхождение товара '{find_item}' имеет индекс {index_item}.")
+    else:
+        print(f"Товар '{find_item}' не найден в списке.")
